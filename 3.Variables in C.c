929a7bef8c166ed5c5a7e439bf7cf1085ad12e4f
PALAVALI MAHESH KUMAR REDDY <itsmepalavalimaheshkumarreddy@gmail.com>
2025-04-05 15:21:42 +0530

    Add files via upload

---
+++ 3.Variables in C.c
@@ -0,0 +1,23 @@
+//C variables
+#include<stdio.h>
+void main(){
+	//declaring a variable
+	int a;
+	//Initialising a variable
+	a=10;
+	float k=10.98;
+	char initial='p';
+	//format specifier
+	printf("%d\n",a);
+	printf("%f\n",k);
+	printf("%c\n",initial);
+	
+	//Change values
+	a=100;
+	printf("%d\n",a);
+	
+	//Multiple Variables
+	
+	int x=10,y=20,z=30;
+	printf("\n%d %d %d",x,y,z);
+}
