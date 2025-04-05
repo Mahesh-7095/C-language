31492bee410468934fc68dc7d36021536d209af4
PALAVALI MAHESH KUMAR REDDY <itsmepalavalimaheshkumarreddy@gmail.com>
2025-04-05 15:16:03 +0530

    Add files via upload

---
+++ 2.Single line and Multi line comments.c
@@ -0,0 +1,8 @@
+//Single line and Multi line comments in c
+#include<stdio.h>
+void main(){
+	//This is single line comment
+	/* This is multi line comment 
+	in two lines */
+	printf("Hello World!");
+}
